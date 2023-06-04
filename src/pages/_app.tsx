b3aa66d443529cb43e0137a7374f47dce9add278
Joao Mateus <mateusgamescoelho547@gmail.com>
2023-06-04 05:54:08 -0300

    main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <AuthProvider>
         <Component {...pageProps} />
-      </AuthProvider>{" "}
+      </AuthProvider>
       <ToastContainer />
     </>
   );
